d7ea1d3fbee20311a4686f09b4ec0ccfa67952cf
Izhar Idrisi <izharidrisi.51@gmail.com>
2025-08-25 21:00:14 +0530

    setup configure

--- src/app.js
+++ src/app.js
@@ -13,6 +13,11 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))  // it receive json data Atmost 16kb
 app.use(express.urlencoded({extended: true, limit: "16kb"})) // it encode url params data 
 app.use(express.static("public"))  // it store static data like image favicon
-app.use(cookieParser())
+app.use(cookieParser());
+
+// routes import
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/users",userRouter)
 
 export { app }
